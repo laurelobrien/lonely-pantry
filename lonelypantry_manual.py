e89f3c16e0cf5e84c5069c8b13f42a5e1b505f1e
Laurel O'Brien <laureljobrien@gmail.com>
2015-05-21 14:33:44 -0700

    used set() to remove duplicate ingredient lists
    set(meal_plan) makes sure a meal’s ingredients are only printed once,
    even if meal is entered twice. ingredients within lists are still
    repeated if the meals they come from are different; #blocked off an
    early attempt at importing itertools to try later.

--- lonelypantry_manual.py
+++ lonelypantry_manual.py
@@ -1,16 +1,22 @@
 # manual list builder
-# turn string input into itemized list
+# TODO: turn string input into itemized list, remove duplicates in grocery list
 
+import itertools
 from lonelypantry_ingredients import *
+
 
 num_meal = int(raw_input("How many meals are you cooking this week? "))
 
 def retrieve_ingredients():
-    meal_plan= []
+    meal_plan = []
+    grocery_list = []
     for count in range(num_meal):
         meal_plan.append(raw_input("Enter meal: "))
+    meal_plan = set(meal_plan)
     for item in meal_plan:
-        print ', '.join(dinner_dict[item])
+        grocery_list.append(dinner_dict[item])
+        #itertools.chain(grocery_list)
+    print grocery_list
 
 retrieve_ingredients()
 
