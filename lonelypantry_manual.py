efdfba5e84d3fa97caddfe1066d6fee419f199a8
Laurel O'Brien <laureljobrien@gmail.com>
2015-05-23 12:12:58 -0700

    merged ingredient lists and formatted the print output
    list.extend() combines the separate ingredient lists retrieved from
    dinner_dict dictionary so the entire grocery_list can be operated on
    like a single list. the final grocery list now also prints with
    capitalized items on their own line.

--- lonelypantry_manual.py
+++ lonelypantry_manual.py
@@ -1,9 +1,7 @@
 # manual list builder
 # TODO: turn string input into itemized list, remove duplicates in grocery list
 
-import itertools
 from lonelypantry_ingredients import *
-
 
 num_meal = int(raw_input("How many meals are you cooking this week? "))
 
@@ -12,11 +10,12 @@ def retrieve_ingredients():
     grocery_list = []
     for count in range(num_meal):
         meal_plan.append(raw_input("Enter meal: "))
-    meal_plan = set(meal_plan)
+    meal_plan = set(meal_plan) #eliminates duplicates
     for item in meal_plan:
-        grocery_list.append(dinner_dict[item])
-        #itertools.chain(grocery_list)
-    print grocery_list
+        grocery_list.extend(dinner_dict[item]) #merges ingredient lists
+    print "Grocery List\n"
+    for item in set(grocery_list):
+        print item.capitalize()
 
 retrieve_ingredients()
 
